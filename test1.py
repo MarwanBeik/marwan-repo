7925844105d2fc094bdc28b1a217e62432e73470
MarwanBeik <114351611+MarwanBeik@users.noreply.github.com>
2022-09-25 20:04:56 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -6,29 +6,31 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(layout="wide")
 
-
-def home(uploaded_file):
-    if uploaded_file:
-        st.header('Begin exploring the data using the menu on the left')
-    else:
-        st.header('To begin please upload a file')
+df = pd.read_csv('2020_report.csv')
+def home(df):
+    st.header('Use the menu on the left to explore the data')
+  
 
 def data_summary():
     st.header('Statistics of Dataframe')
     st.write(df.describe())
 
 def data_header():
-    st.header('Header of Dataframe')
+    st.header('Happiest Countries in EMEA in 2020')
     st.write(df.head())
 
 def displayplot():
-    st.header('GDP per capita in EMEA')
+    
     
     fig = px.bar(df, x='continent', y='gdp_per_capita',
              hover_data=['continent', 'gdp_per_capita'], color='continent',
              height=400)
-    
+    st.header('Distribution of GDP per Capita by Country in EMEA')
+    fig3 = px.bar(df, x='country', y='happiness_score', title = 'Happiness Score per country in EMEA',
+             hover_data=['happiness_score', 'gdp_per_capita'], color='continent',
+             labels={'happiness_score':'Happiness Score'}, height=400)
     st.plotly_chart(fig)
+    st.plotly_chart(fig3)
 
 def interactive_plot():
     
@@ -46,7 +48,7 @@ st.sidebar.title('Sidebar')
 upload_file = st.sidebar.file_uploader('Upload a World Happiness report')
 
 st.sidebar.title('Navigation')
-options = st.sidebar.radio('Select what you want to display:', ['Home', 'Data Summary', 'Data Header', 'GDP Distribution', 'Interactive Plots'])
+options = st.sidebar.radio('Select what you want to display:', ['Home', 'Data Summary', 'Happiest Countries', 'GDP Distribution', 'Interactive Plots'])
 
 
 if upload_file is not None:
@@ -54,10 +56,10 @@ if upload_file is not None:
 
 
 if options == 'Home':
-    home(upload_file)
+    home(df)
 elif options == 'Data Summary':
     data_summary()
-elif options == 'Data Header':
+elif options == 'Happiest Countries':
     data_header()
 elif options == 'GDP Distribution':
     displayplot()
